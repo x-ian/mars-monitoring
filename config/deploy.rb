0bd9584656a31e26c6f1ec6a5cba1c2ddad73813
Christian Neumann <x-ian@x-ian.net>
2012-10-17 20:05:10 +0200

    cap stuff seems to work now

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,18 +9,18 @@
 require 'bundler/capistrano'
 # be sure to change these
 set :user, 'neumann'
-set :domain, 'github.com'
+set :domain, 'www.marsmonitoring.com'
 set :application, 'x-ian/mars-monitoring'
 
 # adjust if you are using RVM, remove if you are not
-require "rvm/capistrano"
-set :rvm_ruby_string, '1.9.3'
-set :rvm_type, :user
+#require "rvm/capistrano"
+#set :rvm_ruby_string, '1.9.3'
+#set :rvm_type, :user
 
 # file paths
-set :repository,  "#{user}@#{domain}:#{application}.git" 
+set :repository,  "git@github.com:x-ian/mars-monitoring.git" 
 #set :deploy_to, "/home/#{user}/#{domain}" 
-set :deploy_to, "/home/neumann/capistrano/mars-monitoring/current/public"
+set :deploy_to, "/home/neumann/capistrano/mars-monitoring"
 
 # distribute your applications across servers (the instructions below put them
 # all on the same server, defined above as 'domain', adjust as necessary)
@@ -29,15 +29,15 @@ role :web, domain
 role :db, domain, :primary => true
 
 # you might need to set this if you aren't seeing password prompts
-# default_run_options[:pty] = true
+default_run_options[:pty] = true
 
 # As Capistrano executes in a non-interactive mode and therefore doesn't cause
 # any of your shell profile scripts to be run, the following might be needed
 # if (for example) you have locally installed gems or applications.  Note:
 # this needs to contain the full values for the variables set, not simply
 # the deltas.
-# default_environment['PATH']='<your paths>:/usr/local/bin:/usr/bin:/bin'
-# default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
+default_environment['PATH']='/usr/local/rvm/gems/ruby-1.9.3-p194/bin:/usr/local/rvm/gems/ruby-1.9.3-p194@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p194/bin:/usr/local/rvm/bin:/usr/local/bin:/usr/bin:/bin'
+default_environment['GEM_PATH']='/usr/local/rvm/gems/ruby-1.9.3-p194:/usr/local/rvm/gems/ruby-1.9.3-p194@global'
 # miscellaneous options
 set :deploy_via, :remote_cache
 set :scm, 'git'
@@ -45,6 +45,7 @@ set :branch, 'master'
 set :scm_verbose, true
 set :use_sudo, false
 set :rails_env, :production
+set :scm_passphrase, "neumann"
 
 namespace :deploy do
   desc "cause Passenger to initiate a restart"
