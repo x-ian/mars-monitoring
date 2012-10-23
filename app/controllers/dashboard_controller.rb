0e5b129a21f0ae8e5fae6cb51ca56c52a7e8e441
Christian Neumann <x-ian@x-ian.net>
2012-10-23 08:59:59 +0200

    syntax error

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
-//    @serviceClassifications = ServiceClassification.all.find_all{|sc| sc.isLeaf?}
-//    @locations = Location.all.find_all{|l| l.isLeaf?}
+#    @serviceClassifications = ServiceClassification.all.find_all{|sc| sc.isLeaf?}
+#    @locations = Location.all.find_all{|l| l.isLeaf?}
   end
   
 end
