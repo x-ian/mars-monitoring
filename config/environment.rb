57317ffb6512fbc06eec9a9235863aa24f9ac71e
Christian Neumann <x-ian@x-ian.net>
2012-12-20 21:53:51 +0100

    tweaks

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,12 @@ MarsMonitoring::Application.initialize!
 
 MarsMonitoring::Application.configure do 
   config.action_mailer.delivery_method = :smtp
-#  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587, 
     domain: "gmail.com",
     authentication: "plain",
-    user_name: "notification@marsgeneral.com",
-    password: "GoingToIbiza",
     enable_starttls_auto: true
   }
 end
