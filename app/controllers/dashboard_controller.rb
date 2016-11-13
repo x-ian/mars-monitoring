9f1e833f606017ff2f482cf7b6e111c15618d24e
x-ian <x-ian@x-ian.net>
2016-11-13 08:54:34 +0100

    d3 dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -46,15 +46,15 @@ class DashboardController < ApplicationController
     tsv += "\n"
 
     # fool d3 as i can't bring the y axis down to 0
-    m = Message.find :first, :order => 'server_time ASC', :conditions => ['probe_id = ? AND server_time >= ? AND server_time <= ?', @probe.id, @start_date, @end_date]
-    unless m.nil?
-    line = "#{m.server_time.strftime("%Y%m%d-%H%M%S")}"
-    line += "\t0\t#{value1_threshold}" if value1_type
-    line += "\t0\t#{value2_threshold}" if value2_type
-    line += "\t0\t#{value3_threshold}" if value3_type
-    line += "\t0\t#{value4_threshold}" if value4_type
-    line += "\n"
-    tsv << line
+#    m = Message.find :first, :order => 'server_time ASC', :conditions => ['probe_id = ? AND server_time >= ? AND server_time <= ?', @probe.id, @start_date, @end_date]
+#    unless m.nil?
+#    line = "#{m.server_time.strftime("%Y%m%d-%H%M%S")}"
+#    line += "\t0\t#{value1_threshold}" if value1_type
+#    line += "\t0\t#{value2_threshold}" if value2_type
+#    line += "\t0\t#{value3_threshold}" if value3_type
+#    line += "\t0\t#{value4_threshold}" if value4_type
+#    line += "\n"
+#    tsv << line
     
     messages = (Message.find :all, :order => 'server_time DESC', :conditions => ['probe_id = ? AND server_time >= ? AND server_time <= ?', @probe.id, @start_date, @end_date], :limit => 10).reverse
     for m in messages
