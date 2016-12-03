b3e0ddbfbb93841ebf0aae3945e53dc0998529bc
x-ian <x-ian@x-ian.net>
2016-12-03 11:51:19 +0100

    trying...

--- app/helpers/probe_indicators_helper.rb
+++ app/helpers/probe_indicators_helper.rb
@@ -1,10 +1,10 @@
 module ProbeIndicatorsHelper
   def table_row_for(element)
-    row = "<td>" + @stats_today[element].to_s + "</td>"
-    row += "<td>" + @stats_yesterday[element].to_s + "</td>"
-    row += "<td>" + @stats_daysago7[element].to_s + "</td>"
-    row += "<td>" + @stats_daysago30[element].to_s + "</td>"
-    row += "<td>" + @stats_ever[element].to_s + "</td>"
+    row = "<td>" + number_with_precision(@stats_today[element], :precision => 2, strip_insignificant_zeros: true).to_s + "</td>"
+    row += "<td>" + number_with_precision(@stats_yesterday[element], :precision => 2, strip_insignificant_zeros: true).to_s + "</td>"
+    row += "<td>" + number_with_precision(@stats_daysago7[element], :precision => 2, strip_insignificant_zeros: true).to_s + "</td>"
+    row += "<td>" + number_with_precision(@stats_daysago30[element], :precision => 2, strip_insignificant_zeros: true).to_s + "</td>"
+    row += "<td>" + number_with_precision(@stats_ever[element], :precision => 2, strip_insignificant_zeros: true).to_s + "</td>"
     return row.html_safe
   end
 end
