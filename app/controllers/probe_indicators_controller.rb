5f5260a48a9c3eb2378fabeac1fabf29c2cc6509
x-ian <x-ian@x-ian.net>
2016-12-03 11:44:11 +0100

    trying...

--- app/controllers/probe_indicators_controller.rb
+++ app/controllers/probe_indicators_controller.rb
@@ -51,8 +51,8 @@ class ProbeIndicatorsController < ApplicationController
       value1_accumulated += m.value1 unless m.value1.nil?
     end
 
-    value1_ratio = ((1 - (value1_above_threshold / (value1_above_threshold + value1_below_threshold))) * 100) unless value1_below_threshold == 0
-    value1_average = value1_accumulated / (alarms + restarts + heartbeats) unless (alarms + restarts + heartbeats) == 0
+    value1_ratio = ((1 - (value1_above_threshold.to_f / (value1_above_threshold + value1_below_threshold))) * 100) unless value1_below_threshold == 0
+    value1_average = value1_accumulated.to_f / (alarms + restarts + heartbeats) unless (alarms + restarts + heartbeats) == 0
     
     stats = { :alarms => alarms, :restarts => restarts, :heartbeats => heartbeats, :value1_above_threshold => value1_above_threshold, :value1_below_threshold => value1_below_threshold, :value1_ratio => value1_ratio, :value1_average => value1_average }
   end
