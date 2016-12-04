552ece66e94613ba0177ef162f0f66b98e0d02a8
x-ian <x-ian@x-ian.net>
2016-12-04 14:22:35 +0100

    a few more things

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ cap production deploy
 bundle exec rake db:seed RAILS_ENV=production --trace
 
 sqlite3 db/develpment.sq3
+
+mysqldump --no-create-info --skip-extended-insert  --complete-insert --skip-add-locks  --compatible=ansi -u neumann marsmonitoring_production messages probe_configurations probe_types probes value_types  | sed "s/\\\'/''/g"  >/tmp/localdb.txt
+Add delete from ... before every new INSERT block
--- app/controllers/probe_indicators_controller.rb
+++ app/controllers/probe_indicators_controller.rb
@@ -4,23 +4,23 @@ class ProbeIndicatorsController < ApplicationController
   def index
     @probe = (params[:probe_id].blank? ? Probe.find(1) : Probe.find(params[:probe_id]))    
 
-    today = Date.today
-    tomorrow = today + 1.day
-    yesterday = today - 1.day
-    daysago7 = today - 7.days
-    daysago30 = today - 30.day
-    ever = today - 5000.day
+    @today = Date.today
+    @tomorrow = @today + 1.day
+    @yesterday = @today - 1.day
+    @daysago7 = @today - 7.days
+    @daysago30 = @today - 30.day
+    @ever = @today - 5000.day
     
     # for today
-    @stats_today = collect_overview(@probe.id, today, tomorrow)
-    # for yesterday
-    @stats_yesterday = collect_overview(@probe.id, yesterday, today)
+    @stats_today = collect_overview(@probe.id, @today, @tomorrow)
+    # for @yesterday
+    @stats_yesterday = collect_overview(@probe.id, @yesterday, @today)
     # for last 7 days
-    @stats_daysago7 = collect_overview(@probe.id, daysago7, today)
+    @stats_daysago7 = collect_overview(@probe.id, @daysago7, @today)
     # for last 30 days
-    @stats_daysago30 = collect_overview(@probe.id, daysago30, today)
-    # for ever
-    @stats_ever = collect_overview(@probe.id, ever, tomorrow)
+    @stats_daysago30 = collect_overview(@probe.id, @daysago30, @today)
+    # for @ever
+    @stats_ever = collect_overview(@probe.id, @ever, @tomorrow)
 
     respond_to do |format|
       format.html
@@ -31,9 +31,7 @@ class ProbeIndicatorsController < ApplicationController
     alarms = 0
     restarts = 0
     heartbeats = 0
-    missing_messages = 0
-    
-    previous_outgoing_message_count = 0
+    missing_messages = 0    
     
     value1_above_threshold = 0
     value1_below_threshold = 0
@@ -41,7 +39,10 @@ class ProbeIndicatorsController < ApplicationController
     value1_accumulated = 0
     value1_average = 0
       
-    messages = Message.where('probe_id = :probe_id AND server_time >= :min_date AND server_time < :max_date', probe_id: probe_id, min_date: min_date, max_date: max_date).order('server_time DESC')
+    messages = Message.where('probe_id = :probe_id AND server_time >= :min_date AND server_time < :max_date', probe_id: probe_id, min_date: min_date, max_date: max_date).order('server_time ASC')
+    
+    previous_outgoing_message_count = 0
+    previous_outgoing_message_count = messages.first.outgoing_message_count unless messages.first.nil?
     
     messages.each do |m|
     
@@ -53,16 +54,29 @@ class ProbeIndicatorsController < ApplicationController
       value1_below_threshold = value1_below_threshold+1 if m.value1_below_threshold?
       value1_accumulated += m.value1 unless m.value1.nil?
       
-      if (previous_outgoing_message_count != 0 && previous_message_count != m.outgoing_message_count + 1)
-        missing_messages = missing_messages + (previous_message_count - m.outgoing_message_count)
+      if (m.outgoing_message_count > previous_outgoing_message_count + 1)
+        missing_messages = missing_messages + (m.outgoing_message_count - previous_outgoing_message_count - 1)
+      elsif (m.outgoing_message_count < previous_outgoing_message_count)
+        if (m.outgoing_message_count == 1 && previous_outgoing_message_count == 255)
+          # 8 bit message counter overflow from 255 to 0
+        else
+          # get missing messages from old counter up to 255
+          missing_messages = missing_messages + (255 - previous_outgoing_message_count)
+          # get missing messages from 0 up to current counter
+          missing_messages = missing_messages + m.outgoing_message_count - 1
+        end
       end
-      previous_message_count = m.outgoing_message_count
+      previous_outgoing_message_count = m.outgoing_message_count
+      
     end
 
     value1_ratio = ((1 - (value1_above_threshold.to_f / (value1_above_threshold + value1_below_threshold))) * 100) unless value1_below_threshold == 0
     value1_average = value1_accumulated.to_f / (alarms + restarts + heartbeats) unless (alarms + restarts + heartbeats) == 0
     
-    stats = { :alarms => alarms, :restarts => restarts, :heartbeats => heartbeats, :value1_above_threshold => value1_above_threshold, :value1_below_threshold => value1_below_threshold, :value1_ratio => value1_ratio, :value1_average => value1_average, :missing_messages => missing_messages, :minutes_in_timeframe => (max_date - min_date) * 60 * 60}
+    minutes_in_timeframe = (max_date.to_time.to_i - min_date.to_time.to_i) / 60
+    
+    expected_heartbeats = minutes_in_timeframe / (Probe.find(probe_id).probe_configuration.heartbeat_interval / 60)
+    stats = { :alarms => alarms, :restarts => restarts, :heartbeats => heartbeats, :value1_above_threshold => value1_above_threshold, :value1_below_threshold => value1_below_threshold, :value1_ratio => value1_ratio, :value1_average => value1_average, :missing_messages => missing_messages, :minutes_in_timeframe => minutes_in_timeframe, :expected_heartbeats=> expected_heartbeats }
   end
   
 end
