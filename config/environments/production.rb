03a491dad763b75d956b8c810ad58bc31f1851d3
x-ian <x-ian@x-ian.net>
2017-02-16 14:34:02 +0100

    actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,6 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -77,6 +73,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587, 
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: YAML.load_file("/home/neumann/capistrano/mars-monitoring/shared/config/environment_gmail_credentials.yml")['user_name'],
+    password: YAML.load_file("/home/neumann/capistrano/mars-monitoring/shared/config/environment_gmail_credentials.yml")['password'],
+    enable_starttls_auto: true
+  }  
+  config.action_mailer.default_url_options = { host: 'www.marsmonitoring.com' }
 
 end
